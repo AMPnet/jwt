ce0427e9fab42078c5d21640371854aadc8912e8
Eugen Druzin <edruzin@gmail.com>
2020-07-12 20:05:18 +0200

    Add tests for JwtReactiveAuthenticationManager

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.test {
